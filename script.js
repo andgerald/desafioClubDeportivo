c79041b7f65948059b372fafbf2d8254b9073b6a
andgeral <geraldinebecerra93@gmail.com>
2024-04-23 13:08:14 -0400

    se agregan validaciones cuando no se encuentra  el nombre o el precio y se valida que  el nombre no este ingresado

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const express = require("express");
 const app = express();
-
+const _ = require("lodash");
 const fs = require("fs");
 const PORT = 3000;
 
 app.get("/agregar", (req, res) => {
   const { nombre, precio } = req.query;
-  const data = { nombre, precio };
+  const club = { nombre, precio };
 
+  //validamos si se ingresaron los valores en el caso que falte un valor arroja el error
   if (!nombre || !precio) {
     return res
       .status(400)
       .send(`Faltan parametros Ingresaste Nombre: ${nombre}, precio:${precio}`);
-  } else {
-    fs.writeFileSync("nuevoClub.json", JSON.stringify(data));
-
-    res.status(200).send("Club agregado con exito!");
   }
+
+  //si pasa la validacion, buscamos si el nombre ingresado se encuentra ya ingresado
+  const { nuevoClub } = JSON.parse(fs.readFileSync("nuevoClub.json", "utf8"));
+  const repetido = _.find(nuevoClub, { nombre });
+
+  //si el nombre ya esta ingresado arroja el siguiene error
+  if (repetido) {
+    return res.status(400).send(`El club ${nombre} ya esta agregado`);
+  }
+
+  //si no esta ingresado, lo agrega y envia el mensaje
+  nuevoClub.push(club);
+  fs.writeFileSync("nuevoClub.json", JSON.stringify({ nuevoClub }));
+  res.status(200).send("Club agregado con exito!");
 });
 
 app.listen(PORT, () => {
